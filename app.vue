f4949197fcd027cf37fae454ea140fb0af31263b
dskymd <dskymd@gmail.com>
2022-06-15 00:00:13 +0900

    nuxt 3

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(123)
+
+const inc = () => {
+  count.value++
+}
+</script>
+
+<template>
+  <div>{{ count }}</div>
+  <button @click="inc">inc</button>
+</template>
